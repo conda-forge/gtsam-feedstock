92be1757c3505adba70bd9bdd980087d9f80f38a
Tobias Fischer <info@tobiasfischer.info>
2022-07-30 07:14:56 +1000

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,11 +3,12 @@ cd build
 
 cmake ^
     -GNinja ^
+    -DGTSAM_BUILD_UNSTABLE:OPTION=OFF ^
     -DGTSAM_BUILD_STATIC_LIBRARY=OFF ^
     -DGTSAM_BUILD_WITH_MARCH_NATIVE=OFF ^
     -DGTSAM_USE_SYSTEM_EIGEN=ON ^
     -DGTSAM_INSTALL_CPPUNITLITE=OFF ^
-    -DGTSAM_BUILD_PYTHON=ON ^
+    -DGTSAM_BUILD_PYTHON=OFF ^
     -DGTSAM_USE_SYSTEM_METIS=ON ^
     -DBoost_USE_STATIC_LIBS=OFF ^
     -DBOOST_ROOT="%LIBRARY_PREFIX%" ^
@@ -22,7 +23,8 @@ cmake ^
 
 if errorlevel 1 exit 1
 
-ninja install
+ninja install -j2
+@rem ninja python-install
 
 if errorlevel 1 exit 1
 
