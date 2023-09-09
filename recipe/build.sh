c0f53e54e70fd07bf91333cda46cea8d818fecd1
Silvio Traversaro <silvio@traversaro.it>
2023-09-09 16:07:10 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -25,23 +25,6 @@ cd python
 $PYTHON -m pip install .
 cd ..
 
-if [ "$(uname)" == "FALSE" ]; then
-  # Detect Python implementation
-  PYTHON_IMPL=$($PREFIX/bin/python -c "import platform; print(platform.python_implementation())")
-
-  if [ "$PYTHON_IMPL" == "CPython" ]; then
-    SO_SUFFIX="cpython-${PY_VER//.}-darwin.so"
-  elif [ "$PYTHON_IMPL" == "PyPy" ]; then
-    SO_SUFFIX="pypy39-pp73-darwin.so"
-  fi
-
-  SO_PATH_GTSAM="$PREFIX/lib/python$PY_VER/site-packages/gtsam/gtsam.$SO_SUFFIX"
-  SO_PATH_UNSTABLE="$PREFIX/lib/python$PY_VER/site-packages/gtsam_unstable/gtsam_unstable.$SO_SUFFIX"
-  install_name_tool -change $SRC_DIR/build/gtsam/libgtsam.4.dylib $PREFIX/lib/libgtsam.4.dylib $SO_PATH_GTSAM
-  install_name_tool -change $SRC_DIR/build/gtsam/libgtsam.4.dylib $PREFIX/lib/libgtsam.4.dylib $SO_PATH_UNSTABLE
-  install_name_tool -change $SRC_DIR/build/gtsam/libgtsam_unstable.4.dylib $PREFIX/lib/libgtsam_unstable.4.dylib $SO_PATH_UNSTABLE
-fi
-
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]] && [[ "$(uname)" != "Darwin" ]]; then
   ninja check
 fi
