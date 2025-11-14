ab281e47dfa33f64871fb8638fa34deec915254e
Tobias Fischer <info@tobiasfischer.info>
2025-11-14 14:30:13 +1000

    Update ninja install command to use CPU_COUNT

--- recipe/build.sh
+++ recipe/build.sh
@@ -28,7 +28,7 @@ cmake .. ${CMAKE_ARGS} \
         -DPython_EXECUTABLE=$PYTHON \
         -DPYTHON_EXECUTABLE=$PYTHON
 
-ninja install -j2
+ninja install -j$CPU_COUNT
 
 cd python
 $PYTHON -m pip install .
